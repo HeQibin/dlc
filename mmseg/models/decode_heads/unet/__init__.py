14742b26009dc9dcf4198217937d3d53ae561ae7
Qibin He <ken_hall@126.com>
2024-01-07 12:03:47 +0800

    delete old code

--- mmseg/models/decode_heads/unet/__init__.py
+++
@@ -1,4 +0,0 @@
-from .unet import Unet
-from .unet_time_embed import Unet as UnetTimeEmbedding
-
-__all__ = ['Unet', 'UnetTimeEmbedding']
--- mmseg/models/necks/__init__.py
+++
@@ -1,9 +0,0 @@
-from .segformer_adapter import SegFormerAdapter
-
-from .multi_stage_merging import MultiStageMerging
-from .fpn import FPN
-
-__all__ = ['SegFormerAdapter',
-           'MultiStageMerging',
-           'FPN',
-           ]
