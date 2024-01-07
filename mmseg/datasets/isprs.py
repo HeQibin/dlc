ba17a78abe4801866be7d2113e5019a7ca4cb2a2
Qibin He <ken_hall@126.com>
2024-01-07 12:06:53 +0800

    update code

---
+++ mmseg/datasets/isprs.py
@@ -0,0 +1,138 @@
+import os.path as osp
+import tempfile
+
+import mmcv
+import numpy as np
+from mmcv.utils import print_log
+from PIL import Image
+
+from .builder import DATASETS
+from .custom import CustomDataset
+
+
+@DATASETS.register_module()
+class ISPRSDataset(CustomDataset):
+    """Cityscapes dataset.
+
+    The ``img_suffix`` is fixed to '_leftImg8bit.png' and ``seg_map_suffix`` is
+    fixed to '_gtFine_labelTrainIds.png' for Cityscapes dataset.
+    """
+
+    CLASSES = ('impervious_surface', 'building', 'low_vegetation', 'tree',
+               'car', 'clutter')
+
+    PALETTE = [[255, 255, 255], [255, 0, 0], [255, 255, 0], 
+               [0, 255, 0], [0, 255, 255], [0, 0, 255]]
+
+    def __init__(self,
+                 crop_pseudo_margins=None,
+                 img_suffix='.png',
+                 seg_map_suffix='.png',
+                 **kwargs):
+        if crop_pseudo_margins is not None:
+            assert kwargs['pipeline'][-1]['type'] == 'Collect'
+            kwargs['pipeline'][-1]['keys'].append('valid_pseudo_mask')
+        super(ISPRSDataset, self).__init__(
+            img_suffix=img_suffix, seg_map_suffix=seg_map_suffix, **kwargs)
+
+        self.pseudo_margins = crop_pseudo_margins
+        self.valid_mask_size = [512, 512]
+
+    def pre_pipeline(self, results):
+        super(ISPRSDataset, self).pre_pipeline(results)
+        if self.pseudo_margins is not None:
+            results['valid_pseudo_mask'] = np.ones(
+                self.valid_mask_size, dtype=np.uint8)
+            # Don't trust pseudo-labels in regions with potential
+            # rectification artifacts. This can lead to a pseudo-label
+            # drift from sky towards building or traffic light.
+            if self.pseudo_margins[0] > 0:
+                results['valid_pseudo_mask'][:self.pseudo_margins[0], :] = 0
+            # Here, the if statement is absolutely necessary
+            if self.pseudo_margins[1] > 0:
+                results['valid_pseudo_mask'][-self.pseudo_margins[1]:, :] = 0
+            if self.pseudo_margins[2] > 0:
+                results['valid_pseudo_mask'][:, :self.pseudo_margins[2]] = 0
+            # Here, the if statement is absolutely necessary
+            if self.pseudo_margins[3] > 0:
+                results['valid_pseudo_mask'][:, -self.pseudo_margins[3]:] = 0
+            results['seg_fields'].append('valid_pseudo_mask')
+
+    def results2img(self, results, imgfile_prefix, to_label_id):
+        """Write the segmentation results to images.
+
+        Args:
+            results (list[list | tuple | ndarray]): Testing results of the
+                dataset.
+            imgfile_prefix (str): The filename prefix of the png files.
+                If the prefix is "somepath/xxx",
+                the png files will be named "somepath/xxx.png".
+            to_label_id (bool): whether convert output to label_id for
+                submission
+
+        Returns:
+            list[str: str]: result txt files which contains corresponding
+            semantic segmentation images.
+        """
+        mmcv.mkdir_or_exist(imgfile_prefix)
+        result_files = []
+        prog_bar = mmcv.ProgressBar(len(self))
+        for idx in range(len(self)):
+            result = results[idx]
+            # if to_label_id:
+            #     result = self._convert_to_label_id(result)
+            filename = self.img_infos[idx]['filename']
+            basename = osp.splitext(osp.basename(filename))[0]
+
+            png_filename = osp.join(imgfile_prefix, f'{basename}.png')
+
+            output = Image.fromarray(result.astype(np.uint8)).convert('P')
+            # import cityscapesscripts.helpers.labels as CSLabels
+            # palette = np.zeros((len(CSLabels.id2label), 3), dtype=np.uint8)
+            # if to_label_id:
+            #     for label_id, label in CSLabels.id2label.items():
+            #         palette[label_id] = label.color
+            # else:
+            #     palette = np.array(self.PALETTE, dtype=np.uint8)
+            palette = np.array(self.PALETTE, dtype=np.uint8)
+
+            output.putpalette(palette)
+            output.save(png_filename)
+            result_files.append(png_filename)
+            prog_bar.update()
+
+        return result_files
+
+    def format_results(self, results, imgfile_prefix=None, to_label_id=True):
+        """Format the results into dir (standard format for Cityscapes
+        evaluation).
+
+        Args:
+            results (list): Testing results of the dataset.
+            imgfile_prefix (str | None): The prefix of images files. It
+                includes the file path and the prefix of filename, e.g.,
+                "a/b/prefix". If not specified, a temp file will be created.
+                Default: None.
+            to_label_id (bool): whether convert output to label_id for
+                submission. Default: False
+
+        Returns:
+            tuple: (result_files, tmp_dir), result_files is a list containing
+                the image paths, tmp_dir is the temporal directory created
+                for saving json/png files when img_prefix is not specified.
+        """
+
+        assert isinstance(results, list), 'results must be a list'
+        assert len(results) == len(self), (
+            'The length of results is not equal to the dataset len: '
+            f'{len(results)} != {len(self)}')
+
+        if imgfile_prefix is None:
+            tmp_dir = tempfile.TemporaryDirectory()
+            imgfile_prefix = tmp_dir.name
+        else:
+            tmp_dir = None
+        result_files = self.results2img(results, imgfile_prefix, to_label_id)
+
+        return result_files, tmp_dir
+
---
+++ mmseg/models/utils/__init__.py
@@ -0,0 +1,14 @@
+from .ckpt_convert import mit_convert
+from .make_divisible import make_divisible
+from .res_layer import ResLayer
+from .self_attention_block import SelfAttentionBlock
+from .shape_convert import nchw_to_nlc, nlc_to_nchw
+
+from .embed import PatchEmbed
+from .transformer import SinePositionalEncoding, DetrTransformerEncoder
+
+__all__ = [
+    'ResLayer', 'SelfAttentionBlock', 'make_divisible', 'mit_convert',
+    'nchw_to_nlc', 'nlc_to_nchw', 'PatchEmbed', 'DetrTransformerEncoder',
+    'SinePositionalEncoding',
+]
